cf159f76f5ead491b0ded2ffee2fa8e3b83d5093
KhanUz <abdullaevolim21@gmail.com>
2025-04-04 20:11:39 +0800

    sqrt by newton

---
+++ 1/1-5.scm
@@ -0,0 +1,13 @@
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+;; applicative order evaluation - first evaluates the argument and then performs the function
+;; normal order evaluation - doesn't compute argument until it is required
+
+;; code execution
+;; infinite loop (recursively) if applicative order
+;; y is not required so it does not compute it (not falling into loop)
---
+++ 1/sqrter.scm
@@ -0,0 +1,21 @@
+(define (sqrter guess x) 
+    (
+        if (valid? guess x)
+            guess
+            (sqrter (modded guess x) x)
+    )    
+)
+
+(define (valid? guess x) 
+    (<  (   abs (- (square guess) x))0.001)
+)
+
+(define (modded guess x)(
+    avg (/ x guess) guess
+))
+(define (square x) (* x x))
+(define (avg x y) (/ (+ x y) 2))
+(define (abs x) (if(> x 0) x (- x)))
+(define (sqrt x) (sqrter 1.0 x))
+(display (sqrt 122))
+(newline)
